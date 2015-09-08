2c5f2a1edb93f39b9f1cb48f2dba451c923f63f2
David & Arturo Perez <pair+NorCalDavid.vrturo@devbootcamp.com>
2015-09-08 11:53:09 -0700

    Cleaned Up RESTful Routes.  Added Login/Logout Routes

--- SmartSync/config/routes.rb
+++ SmartSync/config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
 
-  get 'homepage/index'
+  root 'homepage#index'
 
-root to: 'homepage#index'
+  get    'login'   => 'sessions#new', as: :login
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy', as: :logout
 
+  resources :users do
+
+    member do
+      get 'homes'
+      get 'rooms'
+      get 'devices'
+    end
+
+  end
+   resources :homes do
+      resources :rooms do
+        resources :devices
+      end
+    end
 end
